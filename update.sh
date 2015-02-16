9e8ee38b2ece17656f4303229386aa3d1aca0357
prodge <prodge@prodge.net>
2015-02-16 19:44:10 +0800

    initial version of update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+#Script for pulling the most recent version from github.
+#This will overwrite the previous version.
+
+sudo git clone https://github.com/Prodge/videoModules.git
+cd videoModules/
+sudo mv -f * .[^.]* ..
+cd ..
+sudo rmdir videoModules/
+
