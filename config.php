a21aedbe27a5c84b3393214e7c7c7aeca431af6c
prodge <prodge@prodge.net>
2015-02-16 19:40:40 +0800

    moved install script, commenced work on install script, created update script

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+//SQL Login
+$sqlServer      = "localhost";
+$sqlUsername    = "root";
+$sqlPassword    = "root";
+
+//General
+$domain         = "http://192.168.1.14";
+
+
+?>
+
---
+++ install.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("../config.php");
+
+function query($query){
+    //Create connection
+    $conn = mysqli_connect($sqlServer, $sqlUsername, $sqlPassword);
+    //Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    //Run query
+    if (mysqli_query($conn, $query)) {
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . mysqli_error($conn);
+    }
+
+    //Disconnect
+    mysqli_close($conn);
+}
+    //Creating Database
+    query("CREATE DATABASE videoModules");
+
+    //query("...");
+?> 
