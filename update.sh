713072d38112978e5efb5eb2c8d6337e4716c428
prodge <prodge@prodge.net>
2015-02-16 19:49:30 +0800

    Added delete all files first to clean things up

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #Script for pulling the most recent version from github.
 #This will overwrite the previous version.
 
+sudo rm -rf !(update.sh)
 sudo git clone https://github.com/Prodge/videoModules.git
 cd videoModules/
 sudo mv -f * .[^.]* ..
