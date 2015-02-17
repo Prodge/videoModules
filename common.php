68682a264d25c5dfc065a459e5072a3e8806e32d
prodge <prodge@prodge.net>
2015-02-17 23:36:55 +0800

    added initial version of function pushTable for writing to the database

--- common.php
+++ common.php
@@ -19,6 +19,27 @@ function fetchTable($query){
     while (($row = mysql_fetch_array($result, MYSQL_ASSOC)) !== false){
         $data[] = $row; // add the row in to the results (data) array
     }
+    mysql_close($conn);
     return $data;
+}
+
+function pushTable($query){
+    global $sqlServer;
+    global $sqlDatabase;
+    global $sqlUsername;
+    global $sqlPassword;
+
+    $conn = mysql_connect($sqlServer, $sqlUsername, $sqlPassword);
+    if (!$conn) {
+        die("Connection failed: " . mysql_error());
+
+    }
+    if (!mysql_select_db($sqlDatabase)) die("Could not connect to database: " . mysql_error(    ));
+    
+    //Run query
+    if (!mysql_query($query, $conn)) {
+        die("Query Error: " . mysql_error() . "<br>");
+    }
+    mysql_close($conn);
 }
 ?>
