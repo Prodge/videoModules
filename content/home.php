0d2b623e8ea5997bda8ad4ad8d029c2a8bcdeb46
prodge <prodge@prodge.net>
2015-02-18 22:14:35 +0800

    added support for linked cover images for modules

--- content/home.php
+++ content/home.php
@@ -8,7 +8,7 @@ $modules = fetchTable('select * from modules');
 echo '<table style="width:100%">';
 for($i=0; $i<sizeof($modules); $i++){
     echo '<tr>';
-    echo    '<td width="280px">' . $modules[$i]['thumbnail'] . '</td>';
+    echo    '<td width="280px"><a href="index.php?p=m&i=' . $modules[$i]['id'] . '"><img border="0" src="' . $modules[$i]['thumbnail'] . '" alt="' . $modules[$i]['name'] . '" width="300" height="200"></a></td>';
     echo    '<td width="480px"><a href="index.php?p=m&i=' . $modules[$i]['id'] . '">' . $modules[$i]['name'] . '</a>';
     echo    '<p>' . $modules[$i]['description'] . '</p></td>';
     echo '</tr>';
