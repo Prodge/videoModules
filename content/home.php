85c28e3f6841ad41fdd4859f6752fffd11cc7205
prodge <prodge@prodge.net>
2015-02-18 17:28:26 +0800

    initial working versions of home and module pages

--- content/home.php
+++ content/home.php
@@ -1,1 +1,24 @@
-<p>This is the home page</p>
+<?php
+echo $settings['site_description'];
+echo '<br>';
+
+$modules = fetchTable('select * from modules');
+
+//generating table of modules$modules[$i][''
+echo '<table style="width:100%">';
+for($i=0; $i<sizeof($modules); $i++){
+    if($i % 2 == 0){
+        echo '<tr>';
+    }
+    echo    '<td>' . $modules[$i]['thumbnail'] . '</td>';
+    echo    '<td><a href="index.php?p=m&i=' . $modules[$i]['id'] . '">' . $modules[$i]['name'] . '</a>';
+    echo    '<p>' . $modules[$i]['description'] . '</p></td>';
+    if($i % 2 == 0 && $i != 0){
+        echo '</tr>';
+    }
+}
+echo '</table>';
+
+
+
+?>
--- content/module.php
+++ content/module.php
@@ -1,1 +1,23 @@
-<p>This is a module content area.</p>
+<?php
+$module = fetchTable('select * from modules where id = ' . $id);
+$module = $module[0];
+$videos = fetchTable('select * from videos where module_id = ' . $id . ' order by number');
+
+echo 'Module: ' . $module['name'];
+echo '<br>';
+
+echo '<table style="width:100%">';
+for($i=0; $i<sizeof($videos); $i++){
+    if($i % 3 == 0){
+        echo '<tr>';
+    }
+    echo    '<td>' . $videos[$i]['thumbnail'] . '</td>';
+    echo    '<td><a href="index.php?p=m&i=' . $videos[$i]['id'] . '">' . $videos[$i]['name'] . '</a>';
+    echo    '<p>' . $videos[$i]['description'] . '</p></td>';
+    if($i % 2 == 0 && $i != 0){
+        echo '</tr>';
+    }
+}
+echo '</table>';
+
+?>
