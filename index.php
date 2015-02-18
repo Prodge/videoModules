0df3919e8845f6fc5fe54c9c0c938ea859699970
prodge <prodge@prodge.net>
2015-02-18 17:26:42 +0800

    Minor fixes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 ?>
 <head>
     <title><?php echo $settings["site_name"]; ?></title>
-    <link rel="stylesheet" type="text/css" href="style/main.css">
+    <?php include('style.php">'); ?>
     <meta name="author" content="prodge">
     <meta name="description" content="<?php echo $settings["site_description"]; ?>">
 </head>
