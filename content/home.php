6702ad129fee4199fc36d50d76f0d4d4f45c058e
prodge <prodge@prodge.net>
2015-02-18 20:02:06 +0800

    changed to display modules in one column

--- content/home.php
+++ content/home.php
@@ -7,15 +7,11 @@ $modules = fetchTable('select * from modules');
 //generating table of modules$modules[$i][''
 echo '<table style="width:100%">';
 for($i=0; $i<sizeof($modules); $i++){
-    if($i % 2 == 0){
-        echo '<tr>';
-    }
-    echo    '<td width="130px">' . $modules[$i]['thumbnail'] . '</td>';
-    echo    '<td width="230px"><a href="index.php?p=m&i=' . $modules[$i]['id'] . '">' . $modules[$i]['name'] . '</a>';
+    echo '<tr>';
+    echo    '<td width="280px">' . $modules[$i]['thumbnail'] . '</td>';
+    echo    '<td width="480px"><a href="index.php?p=m&i=' . $modules[$i]['id'] . '">' . $modules[$i]['name'] . '</a>';
     echo    '<p>' . $modules[$i]['description'] . '</p></td>';
-    if($i % 2 == 0 && $i != 0){
-        echo '</tr>';
-    }
+    echo '</tr>';
 }
 echo '</table>';
 
